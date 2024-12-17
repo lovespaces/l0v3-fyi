649eda068cd75511a307541ae5e48956eb013e00
lovespaces <74477945+lovespaces@users.noreply.github.com>
2024-12-17 22:17:14 +0900

    Update top page

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ $("#practico").click(
         window.open("https://l0v3.fyi/keyboard", "_self");
     }
 );
+
+$(document).ready(function() {
+    if ($(window).width() <= 768) {
+        $(".portfolio").removeAttr("id");
+        $("#sokoban").after("<br>");
+        $("#sokoban").css(
+            "margin", "0 auto"
+        );
+        $("#practico").css(
+            "margin", "0 auto"
+        );
+    }
+ });
