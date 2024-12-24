c06f3847f3ddec8ec29690b99e5eef40843f591b
lovespaces <74477945+lovespaces@users.noreply.github.com>
2024-12-24 17:50:11 +0900

    basic.cssの追加、追加に沿ったコードの修正

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-$("#sokoban").click(
-    function() {
-        window.open("https://l0v3.fyi/sokoban", "_self");
-    }
-);
-
-$("#practico").click(
-    function() {
-        window.open("https://l0v3.fyi/keyboard", "_self");
-    }
-);
-
 $(document).ready(function() {
     if ($(window).width() <= 768) {
         $(".portfolio").removeAttr("id");
